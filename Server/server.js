15df8772b31f272b72ee1b90f15a3390ab93ecb7
Lloyd Arnold <lloarn@gmail.com>
2020-06-20 11:37:25 +0000

    improve file readbility

--- Server/server.js
+++ Server/server.js
@@ -4,7 +4,13 @@ var io = require('socket.io')(http);
 
 app.get('/', function(req, res){  res.sendfile('index.html');});
 
-io.on('connection', function(socket){  console.log('user connected');  socket.on('chat message', function(msg){    io.emit('chat message', msg);  });  socket.on('disconnect', function(){    console.log('user disconnected');  });});
+io.on('connection', function(socket){  
+	console.log('user connected');  
+	socket.on('chat message', function(msg){    
+		io.emit('chat message', msg);  });  
+	socket.on('disconnect', function(){    
+		console.log('user disconnected');  });
+});
 
 http.listen(3000, function(){  console.log('listening on *:3000');});
 
