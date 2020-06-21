ecf298add409d603b5bcbeb1e4f9e230d396bf8e
Lloyd Arnold <lloarn@gmail.com>
2020-06-21 15:14:00 +0100

    More advanced server, now palms all the actual work off to controller

--- Server/server.js
+++ Server/server.js
@@ -1,9 +1,9 @@
-/* var express=require('express');
+var express=require('express');
 var app=express();
 var http=require('http').Server(app);
 var io = require('socket.io')(http);
 var ip = require('ip');
-app.use(express.static('./')); 
+app.use(express.static('./'));
 
 require("./Controller/controller.js")(app,io);
 
@@ -14,7 +14,9 @@ require("./Controller/controller.js")(app,io);
 
 http.listen(1337,function(){
     console.log("Node Server is setup and it is listening on http://"+ip.address()+":1337");
-}) */
+})
+
+/* defunct
 
 var app = require('express')();
 var http = require('http').Server(app);
@@ -22,13 +24,13 @@ var io = require('socket.io')(http);
 
 app.get('/', function(req, res){ console.log("got it"); res.send("Hello World"); });
 
-io.on('connection', function(socket){  
-	console.log('user connected');  
-	socket.on('chat message', function(msg){    
-		io.emit('chat message', msg);  });  
-	socket.on('disconnect', function(){    
+io.on('connection', function(socket){
+	console.log('user connected');
+	socket.on('chat message', function(msg){
+		io.emit('chat message', msg);  });
+	socket.on('disconnect', function(){
 		console.log('user disconnected');  });
 });
 
 http.listen(3000, function(){  console.log('listening on *:3000');});
-
+*/
