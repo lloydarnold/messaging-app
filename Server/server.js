892365ab2bcee2ad2e8e6bffebec54c44e917850
Lloyd Arnold <lloarn@gmail.com>
2020-06-21 15:18:34 +0100

    Remove defunct code

--- Server/server.js
+++ Server/server.js
@@ -15,22 +15,3 @@ require("./Controller/controller.js")(app,io);
 http.listen(1337,function(){
     console.log("Node Server is setup and it is listening on http://"+ip.address()+":1337");
 })
-
-/* defunct
-
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-
-app.get('/', function(req, res){ console.log("got it"); res.send("Hello World"); });
-
-io.on('connection', function(socket){
-	console.log('user connected');
-	socket.on('chat message', function(msg){
-		io.emit('chat message', msg);  });
-	socket.on('disconnect', function(){
-		console.log('user disconnected');  });
-});
-
-http.listen(3000, function(){  console.log('listening on *:3000');});
-*/
