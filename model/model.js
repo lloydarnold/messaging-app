a3ce1e5ee13878d40bca4d7cea6852422587f87c
Lloyd Arnold <lloarn@gmail.com>
2020-06-22 12:34:47 +0100

    Annotate database models

--- model/model.js
+++ model/model.js
@@ -17,22 +17,25 @@ mongoose.connection.on('error', function (err) {
 // mongoose.connect('mongodb://localhost/mongodb');
 
 module.exports.user=mongoose.model('User',new Schema({
-    name:String,
-    handle: String,
-    password: String,
-    phone:String,
-    email:String,
-    group:String,
-    primary_contact:String,
-    isAdmin:Boolean,
-    userType:String
+    name:String,              // self explanatory
+    handle: String,           // unique user id
+    password: String,         // self explanatory
+    phone:String,             // ditto
+    email:String,             // primary contact email -- TODO validate this on input (use clientside js)
+    yearGroup:String,         // year group -- TODO increment this every september
+    primary_contact:String,   // mentor or mentee
+    isAdmin:Boolean,          // by default, set to false -- TODO either add admin accounts manually from server OR do something neat
+    userType:String           // mentor or mentee (could set this to boolean isMentor) ?
 },{strict: false}));
 
 module.exports.online=mongoose.model('online',new Schema({
-    handle:String,
-    connection_id:String
+    handle:String,            // keep temporary record of all online users -- this logs their handle
+    connection_id:String      // log connection ID to send messages down socket
 }));
 
+// This is current model for message
+// TODO make messages a nested document
+// top is convo ID (hash of names involved?) and contents is messages sent
 module.exports.messages=mongoose.model('message',new Schema({
     message : String,
     sender  : String,
