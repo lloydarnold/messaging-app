f5492b8412d053b3c027b58bc4bd513c8247a2cd
Lloyd Arnold <lloarn@gmail.com>
2020-06-27 14:16:26 +0100

    Output chat info

--- views/angular/controller.js
+++ views/angular/controller.js
@@ -102,13 +102,18 @@ app.controller('adminController', ['$scope','socket','$http','$mdDialog','$compi
     };
 
     var displayChat = function(chat){
-      var div = document.createElement('div');
+      var div = document.createElement('div');  // TODO fix chat loading
       div.innerHTML='<div>\
-                     <p>' + chat.conversationID + '</p>\
+                     <p> <b> Conversation ID: </b>' + chat.conversationID + '</p>\
+                     <button type="button" class="btn btn-primary btn-flat" ng-click="loadChat('+chat.conversationID+')">View Messages</button>\
                      </div>'
       document.getElementById("convoLog").appendChild(div);
     };
 
+
+    $scope.loadChat = function(convoID) {
+      console.log(convoID);
+    };
 
     $scope.findUser = function(handle) {
       socket.emit('user lookup', handle);
